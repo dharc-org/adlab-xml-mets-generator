dcd49baf9034d8da335319f88650eaf954a3b440
Tommaso <tomm.vitale@gmail.com>
2023-11-16 12:56:39 +0100

    initial improvements

--- adlab-xml-mets-generator.py
+++ adlab-xml-mets-generator.py
@@ -1,7 +1,27 @@
 import json
+import sys
 from openpyxl import load_workbook
 
-wb = load_workbook(filename='../file.xlsx', read_only=True)
+# check arguments
+if len(sys.argv) < 3:
+    print("No parameter has been included")
+    print(" 1) path of json formatted config file i.e. config.json")
+    print(" 1) path of xlsx file with metadata")
+    print("i.e. $ python adlab-xml-mets-generator.py config.json metadata.xlsx")
+    sys.exit()
+
+# get var from arguments
+config = sys.argv[1]
+xslxfile = sys.argv[2]
+
+# get json config file and parse data
+with open(config, "r") as f:
+    configVars = json.load(f)
+
+
+
+# open xlsx in read mode
+wb = load_workbook(filename=xslxfile, read_only=True)
 ws = wb['Sheet1']
 
 for row in ws.rows:
